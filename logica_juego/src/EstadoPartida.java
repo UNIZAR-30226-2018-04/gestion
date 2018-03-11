0666dd82b05e05260c012a33a9274a6862c59cd2
Victor Soria <victorspgl@gmail.com>
2018-03-11 21:16:57 +0100

    "Primer esbozo de EstadoPartida."

--- logica_juego/src/EstadoPartida.java
+++ logica_juego/src/EstadoPartida.java
@@ -4,5 +4,27 @@
  * Fichero: Estado de la partida del guiñote que permite ver las cartas de la partida y sus jugadores
  */
 
+import java.util.ArrayList;
+import Carta
+
 public class EstadoPartida {
+    private List<Jugador> jugadores;
+    private List<Carta> cartasEnTapete;
+    private List<Carta> mazo;
+    private Jugador turno;
+    private Carta triunfo;
+
+    public EstadoPartida(LinkedList<Jugador> jugadores){
+
+    }
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public List<Cartas> getCartas(Jugador jugador){
+        if(jugadores.contains(jugador)){
+            return jugador.getCartasEnMano();
+        }
+    }
 }
